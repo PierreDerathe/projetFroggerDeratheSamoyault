dbd8c53e3db90ebe0e343b0784ff15cbb84025e1
PierreDerathe <pierre.derathe@u-psud.fr>
2021-10-28 14:23:51 +0200

    Correction Frog

--- Frogger/src/frog/Frog.java
+++ Frogger/src/frog/Frog.java
@@ -13,6 +13,12 @@ public class Frog implements IFrog {
 	private Case position;
 	private Direction direction;
 
+	public Frog(Game game){
+		this.game = game;
+		this.position = new Case(game.width/2, 0);
+		this.direction = Direction.up;
+	}
+
 	@Override
 	public Case getPosition(){
 		return position;
@@ -25,11 +31,14 @@ public class Frog implements IFrog {
 
 	@Override
 	public void move(Direction key){
-		position = key == Direction.up? new Case(position.absc, position.ord+1) :
+		Case pos = key == Direction.up? new Case(position.absc, position.ord+1) :
 				key == Direction.down? new Case(position.absc, position.ord-1) :
 						key == Direction.left? new Case(position.absc-1, position.ord) :
 								new Case(position.absc+1, position.ord);
+		if (pos.absc < game.width && pos.absc >= 0 && pos.ord < game.height && pos.ord >= 0) {
+			position = pos;
+			direction = key;
+		}
 	}
-
 
 }
