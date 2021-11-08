a500060eb73c5fbe7f571194625b9127f7d27cdb
BaptisteSamoyault <baptiste.samoyault@u-psud.fr>
2021-11-08 11:23:56 +0100

    finition de Environment

--- Frogger/src/environment/Environment.java
+++ Frogger/src/environment/Environment.java
@@ -1,10 +1,10 @@
 package environment;
 
-import java.util.ArrayList;
-import frog.Frog;
-import util.Case;
 import gameCommons.Game;
 import gameCommons.IEnvironment;
+import util.Case;
+
+import java.util.ArrayList;
 
 public class Environment implements IEnvironment {
     private ArrayList<Lane> road;
@@ -13,11 +13,11 @@ public class Environment implements IEnvironment {
     public Environment(Game game){
         this.game=game;
         this.road= new ArrayList<Lane>();
-        this.road.add(new Game, new Case(game.width, game.height));
-        for(int i=1; i<game.height; i++){
+        this.road.add(new Lane(game, 0, 0));
+        for(int i=1; i<game.height-1; i++){
             this.road.add(new Lane(game, i)); // on va supposer que le constructeur Lane à été déclaré ici et en bas
         }
-        this.road.add(new Lane(game,  game.height - 1, game.height));
+        this.road.add(new Lane(game,  game.height - 1, 0));
     }
 
 
